f10e450572fe68d0f62633c4ef8f6280fdb46fe5
Zakariyya Mughal <zaki.mughal@gmail.com>
2018-03-05 10:10:45 -0500

    Add link to repo info

--- lib/Renard/Jacquard.pm
+++ lib/Renard/Jacquard.pm
@@ -3,3 +3,8 @@ package Renard::Jacquard;
 # ABSTRACT: A high-level structured graphics library
 
 1;
+__END__
+
+=head1 SEE ALSO
+
+L<Repository information|http://project-renard.github.io/doc/development/repo/p5-Renard-Jacquard/>
