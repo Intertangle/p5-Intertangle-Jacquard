d949705a97818d3ac649ce51e0ac7fe60b7ab730
Zakariyya Mughal <zaki.mughal@gmail.com>
2018-03-14 16:08:54 -0400

    Add Image::PNG content

---
+++ lib/Renard/Jacquard/Content/Image/PNG.pm
@@ -0,0 +1,24 @@
+use Renard::Incunabula::Common::Setup;
+package Renard::Jacquard::Content::Image::PNG;
+# ABSTRACT: PNG data content
+
+use Mu;
+use MooX::ClassAttribute;
+use Renard::Incunabula::Common::Types qw(ClassName);
+use Renard::Taffeta::Graphics::Image::PNG;
+
+class_has taffeta_class => (
+	is => 'ro',
+	isa => ClassName,
+	default => 'Renard::Taffeta::Graphics::Image::PNG',
+);
+
+=method BUILDARGS
+
+Takes L<Renard::Taffeta::Graphics::Image::PNG> attributes and stores them.
+
+=cut
+
+with qw(Renard::Jacquard::Content::Role::Graphics::Taffeta);
+
+1;
