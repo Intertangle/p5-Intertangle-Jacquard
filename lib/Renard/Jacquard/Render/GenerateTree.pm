255e472a0a2aebbbf7609b2d1abccab9ddfd5794
Zakariyya Mughal <zaki.mughal@gmail.com>
2018-03-15 08:28:52 -0400

    Add debugging tooltips to SVG output

--- lib/Renard/Jacquard/Render/GenerateTree.pm
+++ lib/Renard/Jacquard/Render/GenerateTree.pm
@@ -60,24 +60,60 @@ method get_render_tree(
 Renders the result of C<get_render_tree> to an SVG file.
 
 =cut
-method render_tree_to_svg( $render_tree, $path ) {
+method render_tree_to_svg( $render_tree, $path  ) {
 	require SVG;
 	my $svg = SVG->new;
 
-	fun walker( $node, $svg ) {
+	method walker( $node, $svg ) {
 		my @daughters = $node->daughters;
 		if( exists $node->attributes->{render} ) {
 			my $el = $node->attributes->{render}->render_svg( $svg );
+			$self->_add_debug_tooltips( $node, $el );
 		}
 		my $group = @daughters > 1 ? $svg->group : $svg ;
 		for my $daughter (@daughters) {
-			walker( $daughter, $group );
+			$self->walker( $daughter, $group );
 		}
 	};
 
-	walker( $render_tree, $svg->group );
+	$self->walker( $render_tree, $svg->group );
 
 	path($path)->spew_utf8($svg->xmlify);
+}
+
+method _add_debug_tooltips( $node, $element ) {
+	require DDP;
+	my %options = (
+		multiline => 0,
+		colored => 0,
+	);
+	my $bounds_str = Data::Printer::np($node->attributes->{bounds}, %options, class => { expand => 'all' } );
+	my $state_str  = Data::Printer::np($node->attributes->{state}->transform->matrix, %options, class => { expand => 'all' } );
+	my $render_str  = Data::Printer::np($node->attributes->{render}, %options, class => { expand => '1' } );
+
+	my $title = $element->tag('title')->cdata_noxmlesc(<<"EOF");
+
+<foreignObject>
+	<body xmlns="http://www.w3.org/1999/xhtml">
+		<code>
+			<pre>
+$state_str
+			</pre>
+		</code>
+		<code>
+			<pre>
+$bounds_str
+			</pre>
+		</code>
+		<code>
+			<pre>
+$render_str
+			</pre>
+		</code>
+</body>
+</foreignObject>
+EOF
+
 }
 
 1;
