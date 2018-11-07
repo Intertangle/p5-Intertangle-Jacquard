874f3212e06f1edf39ed95bff7ccd2e0131e0bba
Zakariyya Mughal <zaki.mughal@gmail.com>
2018-11-07 16:01:54 -0600

    Move layout actor code to Layoutable role

--- lib/Renard/Jacquard/Actor/Taffeta/Group.pm
+++ lib/Renard/Jacquard/Actor/Taffeta/Group.pm
@@ -6,23 +6,7 @@ use Moo;
 
 extends qw(Renard::Jacquard::Actor);
 
-with qw(Renard::Jacquard::Actor::Role::WithChildren);
-
-=attr layout
-
-The layout to use for the children actors.
-
-Predicate: C<has_layout>
-
-=method has_layout
-
-Predicate for C<layout> attribute.
-
-=cut
-has layout => (
-	is => 'ro',
-	predicate => 1,
-);
+with qw(Renard::Jacquard::Actor::Role::Layoutable);
 
 =method bounds
 
@@ -41,13 +25,5 @@ method bounds( $state ) {
 	}
 	return $rect;
 }
-
-after add_child => method( $actor, %options ) {
-	if( $self->has_layout ) {
-		$self->layout->add_actor( $actor,
-			exists $options{layout} ? %{ $options{layout} } : ()
-		);
-	}
-};
 
 1;
