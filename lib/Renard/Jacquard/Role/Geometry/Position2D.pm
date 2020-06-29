a3c83a0812fb40fd92121fffee5b38fa7595425d
Zakariyya Mughal <zaki.mughal@gmail.com>
2020-06-28 22:48:32 -0500

    Add 2D geometry roles

---
+++ lib/Renard/Jacquard/Role/Geometry/Position2D.pm
@@ -0,0 +1,11 @@
+use Renard::Incunabula::Common::Setup;
+package Renard::Jacquard::Role::Geometry::Position2D;
+# ABSTRACT: «TODO»
+
+use Mu::Role;
+use Renard::Punchcard::Attributes;
+
+variable x =>;
+variable y =>;
+
+1;
---
+++ lib/Renard/Jacquard/Role/Geometry/Size2D.pm
@@ -0,0 +1,11 @@
+use Renard::Incunabula::Common::Setup;
+package Renard::Jacquard::Role::Geometry::Size2D;
+# ABSTRACT: «TODO»
+
+use Mu::Role;
+use Renard::Punchcard::Attributes;
+
+variable width =>;
+variable height =>;
+
+1;
