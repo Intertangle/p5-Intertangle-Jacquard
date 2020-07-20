f3afd9e96b349d98387bd76a2f94508504868736
Zakariyya Mughal <zaki.mughal@gmail.com>
2020-07-19 23:51:31 -0500

    Add method to compute origin from attributes

--- lib/Renard/Jacquard/Role/Geometry/Position2D.pm
+++ lib/Renard/Jacquard/Role/Geometry/Position2D.pm
@@ -4,8 +4,16 @@ package Renard::Jacquard::Role::Geometry::Position2D;
 
 use Mu::Role;
 use Renard::Punchcard::Attributes;
+use Renard::Yarn::Graphene;
 
 variable x =>;
 variable y =>;
+
+method origin_point() {
+	Renard::Yarn::Graphene::Point->new(
+		x => $self->x->value,
+		y => $self->y->value,
+	);
+}
 
 1;
